8278eb8475acfea562aa444636dc94ebdfdea602
Derek Barnes <github-drb@drbproductions.com>
2021-04-02 11:00:21 -0600

    First Commit

---
+++ source/js/main.js
@@ -0,0 +1,11 @@
+// @codekit-prepend "../../vendor/jquery-3.5.1.min.js","../../vendor/bootstrap-4.5.0/dist/js/bootstrap.bundle.min.js","../../vendor/slick-1.9.0/slick/slick.min.js"
+
+
+$(document).ready(function(){
+  $('.your-class').slick({
+    autoplay: true,
+    dots: true
+  });
+});
+
+   
